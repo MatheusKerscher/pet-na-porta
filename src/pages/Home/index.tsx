bc444b2bd780d94788551a870c55176405c9af40
MatheusKerscher <kerschermatheus12@gmail.com>
2025-04-21 10:18:58 -0300

    Spinner and initial home page created

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import Spinner from "../../components/Spinner/indext";
+
+
+
 const Home = () => {
-  return ( <></> );
-}
- 
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  if (loading) {
+    return (
+      <Spinner tip="Buscando produtos..."/>
+    );
+  }
+
+  return (
+    <main className="w-full h-[calc(100vh-60px)] px-6">
+      <section className="w-full"></section>
+
+      <div className="w-full">
+        <section></section>
+        <section></section>
+      </div>
+    </main>
+  );
+};
+
 export default Home;
